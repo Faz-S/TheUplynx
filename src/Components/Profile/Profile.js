93520b61bd9758f007d5391d1b04a551a5d0a919
Fazil <fazilsamysha2005@gmail.com>
2024-05-11 18:45:21 +0530

    Second Commit

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,11 +1,75 @@
 import React from 'react'
-
+import './Profile.css'
+import log from './das.jpeg'
+import Home from '../Home/Home'
 function Profile() {
-  return (
+  return (<>
     <div>
+      <Home/>
+    </div>
+    <div className='themaincont'>
+    <div className="profile">
+      <div>
+        <p>24K</p>
+        <p style={{marginLeft:"-1rem"}}>Followers</p>
+      </div>
+      <div>
+        <img className="img234"src={log}></img>
+        
+        <p className='name'>Das</p>
+        
+       
+        <p style={{marginLeft:"-1.9rem"}}>fazilsamysha2005@gmail.com</p>
+      </div>
+      <div>
+        <p>154</p>
+        <p style={{marginLeft:"-1rem"}}>Following</p>
+      </div>
       
     </div>
-  )
+    <div className='class'>
+    <p>I exist to design pixels, beyond that my life is void and meaningless... i'm just kidding I live to make other peoples lives easier.</p>
+    </div >
+    <div className='yaay'>
+        <h3 className='h3'>My Stats</h3>
+        <div className='saicard'>
+        <div className='blackdiv'>
+            <a className='fas fa-home'></a>
+            <h3>56.4k</h3>
+            <p>Customer</p>
+        </div>
+        <div className='blackdiv'>
+            <a className='fas fa-home'></a>
+            <h3>56.4k</h3>
+            <p>Customer</p>
+        </div>
+        <div className='blackdiv'>
+            <a className='fas fa-home'></a>
+            <h3>56.4k</h3>
+            <p>Customer</p>
+        </div>
+        </div>
+        <div className='saicard'>
+        <div className='blackdiv'>
+            <a className='fas fa-home'></a>
+            <h3>56.4k</h3>
+            <p>Customer</p>
+        </div>
+        <div className='blackdiv'>
+            <a className='fas fa-home'></a>
+            <h3>56.4k</h3>
+            <p>Customer</p>
+        </div>
+        <div className='blackdiv'>
+            <a className='fas fa-home'></a>
+            <h3>56.4k</h3>
+            <p>Customer</p>
+        </div>
+        </div>
+
+    </div>
+    </div>
+    </>)
 }
 
 export default Profile
--- src/Components/Store/Store.js
+++ src/Components/Store/Store.js
@@ -1,11 +1,131 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import './Store.css'
+import logo4 from './austin.jpg'
+import logo5 from './back.jpg'
+import logo6 from './sky.jpg'
+import Home from '../Home/Home'
 function Store() {
-  return (
+    useEffect(()=>{
+       document.body.classList.add("body_no_2") 
+    })
+  return (<>
     <div>
-      
+        <Home/>
+        <div className='storebdy'>
+        <div className='static_nav'>
+            <p className='uplynx'>UPLYNX</p>
+            <a  className="atag "href="/Startup">STARTUP</a>
+        </div>
+        <div className='innerstrbdy'>
+            <input type='text' placeholder='Search'></input>
+            <p>Category Titles</p>
+            <div className='btndiv'>
+                <button>AI News</button>
+                <button>AR/VR</button>
+                <button>Blockchain</button>
+                <button>ML/DL</button>
+            </div>
+            <div className='card_div'>
+                <div>
+                    <img src={logo4}></img>
+                    <p>Hot news</p>
+                    <h4>Title</h4>
+                </div>
+                <div>
+                    <img src={logo5}></img>
+                    <p>Hot news</p>
+                    <h4>Title</h4>
+                </div>
+                <div>
+                    <img src={logo6}></img>
+                    <p>Hot news</p>
+                    <h4>Title</h4>
+                </div>
+            </div>
+            
+            <div className='mmain_div'>
+                <div className='ggboys'>
+                    <p>Suggested</p>
+                    <h3>Suggestion</h3>
+                    <p className='onlyp' >
+                    this is picture taken by the ai developed by the microsft Organization
+                    </p>
+                    
+                </div>
+                <div>
+                    <img  className="clsimg"src={logo6}></img>
+                </div>
+            </div>
+            <p>Category Titles</p>
+            <div className='container_of_card'>
+            <div className='card2_div'>
+                <div>
+                <img className="image"src={logo5}></img>
+                </div>
+                <div>
+                <p>Location,Address</p>
+                <h4>Title</h4>
+                <p>A small description</p>
+                </div>
+            </div>
+            <div className='card2_div'>
+                <div>
+                <img className="image"src={logo4}></img>
+                </div>
+                <div>
+                <p>Location,Address</p>
+                <h4>Title</h4>
+                <p>A small description</p>
+                </div>
+            </div>
+            <div className='card2_div'>
+                <div>
+                <img className="image"src={logo6}></img>
+                </div>
+                <div>
+                <p>Location,Address</p>
+                <h4>Title</h4>
+                <p>A small description</p>
+                </div>
+            </div>
+            </div>
+            <div className='container_of_card'>
+            <div className='card2_div'>
+                <div>
+                <img className="image"src={logo6}></img>
+                </div>
+                <div>
+                <p>Location,Address</p>
+                <h4>Title</h4>
+                <p>A small description</p>
+                </div>
+            </div>
+            <div className='card2_div'>
+                <div>
+                <img className="image"src={logo5}></img>
+                </div>
+                <div>
+                <p>Location,Address</p>
+                <h4>Title</h4>
+                <p>A small description</p>
+                </div>
+            </div>
+            <div className='card2_div'>
+                <div>
+                <img className="image"src={logo4}></img>
+                </div>
+                <div>
+                <p>Location,Address</p>
+                <h4>Title</h4>
+                <p>A small description</p>
+                </div>
+            </div>
+            </div>
+        </div>
+        
     </div>
-  )
+    </div>
+  </>)
 }
 
 export default Store
